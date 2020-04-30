42666132947049f8d44452ba24145d2a625c388a
PurpleBubble <boyawu686@163.com>
2020-04-30 17:36:40 +0200

    decorator

--- o_notation/decorator.py
+++ o_notation/decorator.py
@@ -66,4 +66,85 @@ def function_demo2(args):
 
 
 ######################
-# eg.3
+# eg.3 多个参数
+def many_args(function_name):
+    def warpper(*args):
+        print(*args)
+        function_name(*args)
+    return warpper
+
+
+@many_args
+def function_demo3(*args):
+    print(*args)
+
+
+#function_demo3(1,2,3,4,5,6)
+
+
+########################
+# eg.4 key:value类型参数
+
+def dict_args(function_name):
+    def wrapper(**kwargs):
+        print(kwargs)
+        function_name(**kwargs)
+    return wrapper
+
+
+@dict_args
+def function_demo4(**kwargs):
+    print(kwargs)
+
+
+#function_demo4(name = "aaa", age = 10)
+
+
+#########################
+# eg.5 组合类型参数
+
+def combo_args(function_name):
+    def w(*args, **kwargs):
+        print(args,kwargs)
+    return w
+
+
+@combo_args
+def function_demo5(*args, **kwargs):
+    pass
+
+
+#function_demo5(1,2, name = "aaa", age = 10)
+
+
+##########################
+# eg.6 终极：最大运行时间
+
+
+def max_runtime(timeout):
+    def out_wrapper(function_name):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            i = function_name()
+            end_time = time.time()
+            use_time = end_time - start_time
+
+            if use_time >timeout:
+                print("函数运行超时")
+                #raise RuntimeError ("函数运行超时")
+            return i # 函数返回值
+        return wrapper
+    return out_wrapper
+
+
+
+
+@max_runtime(timeout = 3)
+def function_demo6(*args, **kwargs):
+    time.sleep(2)
+    print("demo6运行")
+    return  1
+
+
+r = function_demo6()
+print(r)
---
+++ o_notation/function_args.py
@@ -0,0 +1,8 @@
+# It is a nice day. Let's get start
+# @Author : Boya
+# @Time   : 30/04/2020 17.27
+
+"""
+函数的参数
+"""
+
